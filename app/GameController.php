b6659ffafc3a497b50b97a724c46b55d7883ec5b
Jed-Meneur <g9fj87zphs@privaterelay.appleid.com>
2026-08-18 16:41:50 +0200

    Add files via upload

---
+++ app/GameController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GameController extends Controller
+{
+    public function index()
+    {
+        return view('game');
+    }
+
+    public function final()
+    {
+        return view('final');
+    }
+}
